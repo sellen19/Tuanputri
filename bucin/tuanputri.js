ffb17de52996450364a846a94a02796515d23ef6
SI BINS <setiawansellen12@gmail.com>
2025-03-18 06:25:00 +0700

    Update tuanputri.js

--- bucin/tuanputri.js
+++ bucin/tuanputri.js
@@ -170,7 +170,7 @@ function muncultombol2() {
 function menuju() {
   if (fungtom2 == 1) {
     window.location =
-      "https://wa.me/62881036475301?text=" + encodeURIComponent(pesanwhatsapp);
+      "https://wa.me/62881036475301" + (pesanwhatsapp);
   }
 }
 
